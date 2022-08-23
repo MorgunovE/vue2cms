dd64175f6ba9445b6cfb259575e69fb3183512a5
MorgunovE <morgunove87@gmail.com>
2022-08-23 17:18:45 +0400

    pagination

--- src/views/History.vue
+++ src/views/History.vue
@@ -8,36 +8,47 @@
       <canvas></canvas>
     </div>
     <Loader v-if="loading"/>
-    <p class="center" v-else-if="!records.length">Haven't records <router-link to="/record">Add new record</router-link></p>
+    <p class="center" v-else-if="!records.length">Haven't records
+      <router-link to="/record">Add new record</router-link>
+    </p>
     <section v-else>
-      <HistoryTable :records="records"/>
+      <HistoryTable :records="items"/>
+      <Paginate
+        v-model="page"
+        :page-count="pageCount"
+        :click-handler="pageChangeHandler"
+        :prev-text="'Prev'"
+        :next-text="'Next'"
+        :container-class="'pagination'"
+        :page-class="'waves-effect'"
+      />
     </section>
   </div>
 </template>
 
 <script>
   import HistoryTable from '@/components/HistoryTable'
+  import paginationMixin from '@/mixins/pagination.mixin'
   
   export default {
     name: 'history',
+    mixins: [paginationMixin],
     data() {
       return {
         loading: true,
-        records: [],
-        categories: []
+        records: []
       }
     },
     async mounted() {
-      // this.records = await this.$store.dispatch('fetchRecords')
-      const records = await this.$store.dispatch('fetchRecords')
-      this.categories = await this.$store.dispatch('fetchCategories')
-      this.records = records.map(record => {
+      this.records = await this.$store.dispatch('fetchRecords')
+      const categories = await this.$store.dispatch('fetchCategories')
+      this.setupPagination(this.records.map(record => {
         return {
           ...record,
-          categoryName: this.categories.find(category => category.id === record.categoryId).title,
+          categoryName: categories.find(category => category.id === record.categoryId).title,
           typeClass: record.type === 'income' ? 'green' : 'red',
         }
-      })
+      }))
       this.loading = false
     },
     components: {HistoryTable},
